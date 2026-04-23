33d2fdc76b00548ff315835077f4bd696b54e601
save-on <sedmon033@gmail.com>
2026-04-23 18:26:52 -0500

    add webcam functionality

--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,51 @@
 #include <iostream>
 #include <unistd.h>
 
+struct CustomData {
+    GstElement *pipeline, *source, *sink, *convert;
+};
+
 int main(int argc, char *argv[]) {
-    handleBanner();
-    GstElement          *pipeline, *source, *sink;
+    // handleBanner();
+
+    CustomData           data;
     GstBus              *bus;
     GstStateChangeReturn ret;
 
     gst_init(&argc, &argv);
 
-    source   = gst_element_factory_make("videotestsrc", NULL);
-    sink     = gst_element_factory_make("autovideosink", NULL);
-    pipeline = gst_pipeline_new("testpipeline");
+    /*
+        attach a webcam source
+        attach a screen sink
+    */
 
-    if (!pipeline || !source || !sink) {
+    data.source   = gst_element_factory_make("v4l2src", NULL);
+    data.convert  = gst_element_factory_make("videoconvert", NULL);
+    data.sink     = gst_element_factory_make("xvimagesink", NULL);
+    data.pipeline = gst_pipeline_new("testpipeline");
+
+    if (!data.pipeline || !data.source || !data.convert || !data.sink) {
         std::cout << "not all elements could be created\n";
         return 1;
     }
 
-    g_object_set(source, "pattern", 18, NULL);
-    g_object_set(source, "background-color", "0x0000ff00", NULL);
-    gst_bin_add_many((GstBin *)(pipeline), source, sink, NULL);
-    if (gst_element_link(source, sink) != TRUE) {
+    gst_bin_add_many((GstBin *)(data.pipeline), data.source, data.convert, data.sink, NULL);
+    if (gst_element_link_many(data.source, data.convert, data.sink, NULL) != TRUE) {
         std::cout << "elements could not be linked\n";
-        gst_object_unref(pipeline);
+        gst_object_unref(data.pipeline);
         return 1;
     }
 
-    ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    ret = gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
     if (ret == GST_STATE_CHANGE_FAILURE) {
-        std::cout << "could not change pipeline state to playing\n";
-        gst_object_unref(pipeline);
+        std::cout << "could not change data.pipeline state to playing\n";
+        gst_object_unref(data.pipeline);
         return 1;
     }
 
     sleep(10);
 
-    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_element_set_state(data.pipeline, GST_STATE_NULL);
 
     return 0;
 }
