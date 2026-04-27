6f2bb94e1cd645e82eea3d2cef8f5dfcdcfc5ee5
save-on <sedmon033@gmail.com>
2026-04-26 23:06:21 -0500

    add elements to multithreaded pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -4,53 +4,90 @@
 #include <iostream>
 #include <unistd.h>
 
-struct CustomData {
-    GstElement *pipeline, *source, *sink, *videorate;
+struct VideoData {
+    GstElement *videorate, *queue;
 };
+
+struct AudioData {
+    GstElement *convert, *queue, *resample;
+};
+
+/*
+    > Audio fix
+    - seems like I'll have to build a multithreaded pipeline
+    - we'll need a "tee" probably a demuxer (tee)
+    - we'll want a queue to pause data stream until have enough bytes (queue)
+    - audio will probably need a convert (audioconvert)
+    - add a audio resampler (audioresample)
+    - current video pipeline should be fine
+    - in order to make sure audio and video are on the same with timestamps <--
+    - need a funnel to bring pipeline back to filesink
+
+                          --- PIPELINE VISUAL DRAFT ---
+                     __queue__audioconvert__audioresample__
+                    /                                      \
+     v4l2src--tee---                                        ---funnel--filesink
+                    \__queue____________________videorate__/
+*/
 
 int main(int argc, char *argv[]) {
     // handleBanner();
 
-    CustomData           data;
-    GstBus              *bus;
+    VideoData videoData;
+    AudioData audioData;
+    // GstBus              *bus;
     GstStateChangeReturn ret;
+    GstElement          *pipeline, *tee, *source, *sink, *funnel;
 
     gst_init(&argc, &argv);
 
-    /*
-        attach a webcam source
-        attach a screen sink
-    */
+    // clang-format off
+    pipeline = gst_pipeline_new("anchor_pipeline");
+                                                    // video/audio source
+    source              = gst_element_factory_make("v4l2src", "VA_source");
+    tee                 = gst_element_factory_make("tee", "tee");
+    videoData.queue     = gst_element_factory_make("queue", "video_queue");
+    audioData.queue     = gst_element_factory_make("queue", "audio_queue");
+    videoData.videorate = gst_element_factory_make("videorate", "video_rate");
+    audioData.convert   = gst_element_factory_make("audioconvert", "audio_converter");
+    audioData.resample  = gst_element_factory_make("audioresample", "audio_resampler");
+    funnel              = gst_element_factory_make("funnel", "funnel");
+    sink                = gst_element_factory_make("filesink", "file_output");
+    // clang-format on
 
-    data.source    = gst_element_factory_make("v4l2src", NULL);
-    data.videorate = gst_element_factory_make("videorate", NULL);
-    data.sink      = gst_element_factory_make("filesink", NULL);
-    data.pipeline  = gst_pipeline_new("anchor_pipeline");
-
-    if (!data.pipeline || !data.source || !data.videorate || !data.sink) {
+    if (!pipeline || !source || !videoData.videorate || !sink) {
         std::cout << "not all elements could be created\n";
         return 1;
     }
 
-    gst_bin_add_many((GstBin *)(data.pipeline), data.source, data.videorate, data.sink, NULL);
-    if (gst_element_link_many(data.source, data.videorate, data.sink, NULL) != TRUE) {
+    // clang-format off
+    gst_bin_add_many(
+            (GstBin *)(pipeline), 
+            source, 
+            videoData.videorate, 
+            sink,
+            NULL
+            );
+    // clang-format on
+
+    if (gst_element_link_many(source, videoData.videorate, sink, NULL) != TRUE) {
         std::cout << "elements could not be linked\n";
-        gst_object_unref(data.pipeline);
+        gst_object_unref(pipeline);
         return 1;
     }
-    g_object_set(data.sink, "location", "../output/video.mp4", NULL);
-    g_object_set(data.source, "do-timestamp", TRUE, NULL);
+    g_object_set(sink, "location", "../output/video.mp4", NULL);
+    g_object_set(source, "do-timestamp", TRUE, NULL);
 
-    ret = gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
+    ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
     if (ret == GST_STATE_CHANGE_FAILURE) {
-        std::cout << "could not change data.pipeline state to playing\n";
-        gst_object_unref(data.pipeline);
+        std::cout << "could not change pipeline state to playing\n";
+        gst_object_unref(pipeline);
         return 1;
     }
 
     sleep(10);
 
-    gst_element_set_state(data.pipeline, GST_STATE_NULL);
+    gst_element_set_state(pipeline, GST_STATE_NULL);
 
     return 0;
 }
